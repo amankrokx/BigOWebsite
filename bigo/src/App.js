1bcc1d23203fcb062706ab4037e4ccde37c6b329
AUB <adit.ignite@gmail.com>
2023-05-19 02:11:43 +0530

    contact section done with server

--- bigo/src/App.js
+++ bigo/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Events } from "./components/Events";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Events />
+      <Contact />
     </div>
   );
 }
