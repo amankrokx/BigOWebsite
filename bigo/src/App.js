c35c1551a597a46eef1f24f00c9a77850454ed92
AUB <adit.ignite@gmail.com>
2023-05-18 17:23:53 +0530

    first commit

---
+++ bigo/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Events } from "./components/Events";
+
+function App() {
+  return (
+    <div className="App">
+      <Events />
+    </div>
+  );
+}
+
+export default App;
