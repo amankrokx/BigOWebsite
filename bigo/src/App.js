d477a3bcc9301e9556bcc2030f2f0a85b1978b75
AUB <adit.ignite@gmail.com>
2023-05-19 15:46:11 +0530

    navbar, banner, footer done

--- bigo/src/App.js
+++ bigo/src/App.js
@@ -1,16 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
 import { Events } from "./components/Events";
 import { Contact } from "./components/Contact";
 import { Members } from "./components/Members";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Banner />
       <Events />
       <Members />
       <Contact />
+      <Footer />
     </div>
   );
 }
