def6e7a32d4738680e7227b9e62a07a866793037
Aman KUmar <amankumar.spj410@gmail.com>
2023-05-21 01:53:55 +0530

    final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,7 +36,7 @@ export const Contact = () => {
     let result = await response.json();
     setButtonText("Send");
     setFormDetails(formInitialDetails);
-    if (result.code == 200) {
+    if (result.code === 200) {
       setStatus({ success: true, message: "Message sent successfully" });
     } else {
       setStatus({
